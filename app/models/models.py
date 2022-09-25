a03676a6831c0fa8495f4184ca825eb7a982125a
Guilherme Correa <78979736+guilhermedcorrea@users.noreply.github.com>
2022-09-25 17:05:47 -0300

    Update models.py

--- app/models/models.py
+++ app/models/models.py
@@ -4,78 +4,76 @@ from flask_sqlalchemy import SQLAlchemy
 #Imports Modulos
 from ..extensions import db
 
+
 class Usuarios(db.Model):
     __tablename__ = 'usuarios'
-    __table_args__ = {"schema": "usuarios"}
     id_usuario = db.Column(db.Integer,  primary_key = True)
-    id_grupo = db.Column(db.Integer)
     nome =db.Column(db.String(255))
-    email =db.Column(db.String(255))
     password_hash= db.Column(db.String(255))
     bitusuario = db.Column(db.Boolean, nullable=False)
     bitlogado = db.Column(db.Boolean, nullable=False)
-    datalogado = db.Column(db.DateTime, unique=False, nullable=False)
-    datacadastro = db.Column(db.DateTime, unique=False, nullable=False)
-    grupo = db.Column(db.String(255))
-    bitativo = db.Column(db.String(255))
+    datalogado = db.Column(db.DateTime, unique=False, nullable=True)
+    datacadastro = db.Column(db.DateTime, unique=False, nullable=True)
+    bitativo = db.Column(db.Boolean)
 
     def __repr__(self):
-        return self.id_usuario
+        return  f'<Nome>"{self.id_usuario}"'
 
  
 class MarcaProduto(db.Model):
     __tablename__ = 'marcaproduto'
-    __table_args__ = {"schema": "produtos"}
     idmarca = db.Column(db.Integer,  primary_key = True)
-    marca = db.Column(db.Integer,  primary_key = True)
-    bitativo = db.Column(db.Boolean, nullable=False)
-    datacadastro = db.Column(db.DateTime, unique=False, nullable=False)
+    marca = db.Column(db.String(100))
+    bitativo = db.Column(db.Boolean, nullable=True)
+    datacadastro = db.Column(db.DateTime, unique=False, nullable=True)
+    bitativo = db.Column(db.Boolean)
+    produtos = db.relationship('skuproduto', backref='idmarca')
+    frete = db.relationship('marca', backref='idmarca')
 
     def __repr__(self):
-        return self.idmarca
+        return  f'<Marca>"{self.marca}"'
 
 
 class Produtos(db.Model):
     __tablename__ = 'produto'
-    __table_args__ = {"schema": "produtos"}
     idproduto = db.Column(db.Integer, primary_key = True)
-    skuproduto = db.Column(db.Integer,unique=False , nullable=True)
-    nomeproduto =db.Column(db.String(255),unique=False , nullable=True)
-    eanproduto = db.Column(db.String(255),unique=False , nullable=True)
-    categoriaproduto = db.Column(db.String(255),unique=False , nullable=True)
-    idmarca = db.Column(db.Integer,unique=False , nullable=True)
-    sku = db.Column(db.String(255),unique=False , nullable=True)
+    skuproduto = db.Column(db.String(300),unique=False , nullable=True)
+    nomeproduto =db.Column(db.String(1000),unique=False , nullable=True)
+    idmarca = db.Column(db.Integer,db.ForeignKey('marcaproduto.idmarca'))
     urlpaginaproduto = db.Column(db.String(2000),unique=False , nullable=True)
     peso = db.Column(db.Float,unique=False , nullable=True)
     altura = db.Column(db.Float,unique=False , nullable=True)
     largura = db.Column(db.Float,unique=False , nullable=True)
-    bitativo = db.Column(db.Float,unique=False ,nullable=True)
+    bitativo = db.Column(db.Boolean,unique=False ,nullable=True)
     dataalterado = db.Column(db.DateTime, unique=False, nullable=True)
+    frete = db.relationship('idproduto', backref='idproduto')
 
     def __repr__(self):
-        return self.idproduto
+        return  f'<SKU>"{self.idproduto}"'
 
 class CotacaoFrete(db.Model):
     __tablename__ = 'cotacaofrete'
-    __table_args__ = {"schema": "fretes"}
     idfrete = db.Column(db.Integer, primary_key = True)
-    cep = db.Column(db.String)
+    idmarca = db.Column(db.Integer,db.ForeignKey('marcaproduto.idmarca')) #adicionar
+    idproduto = db.Column(db.Integer,db.ForeignKey('produto.idproduto'))#adicionar
+    cep = db.Column(db.String(20))
+    categoriafrete = db.Column(db.String(50))
     valorFrete = db.Column(db.Float)
     prazo = db.Column(db.Integer)
-    transportadora = db.Column(db.String)
-    dacotacao = db.Column(db.DateTime, unique=False, nullable=False)
+    transportadora = db.Column(db.String(200))
+    dacotacao = db.Column(db.DateTime, unique=False, nullable=True)
+    log = db.relationship('idfrete', backref='idfrete')
 
     def __repr__(self):
-        return self.idfrete
-
+        return f'<idfrete>"{self.idfrete}"'
 
 class LogUsuario(db.Model):
     __tablename__ = 'logusuario'
-    __table_args__ = {"schema": "usuarios"}
     idlog = db.Column(db.Integer, primary_key = True)
-    idusuario = db.Column(db.Integer)
-    datalog = db.Column(db.DateTime, unique=False, nullable=False)
+    idusuario = db.Column(db.Integer,db.ForeignKey('usuarios.id_usuario'))
+    idfrete = db.Column(db.Integer, db.ForeignKey('cotacaofrete.idfrete'))
+    datalog = db.Column(db.DateTime, unique=False, nullable=True)
 
     def __repr__(self):
-        return self.idlog
+        return f'<idfrete>"{self.idlog}"'
 
