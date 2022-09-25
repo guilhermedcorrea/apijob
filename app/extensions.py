e196c17255f382a230912b27d77941dd3cf0b79f
Guilherme Correa <78979736+guilhermedcorrea@users.noreply.github.com>
2022-09-25 20:49:18 -0300

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_restx import Api
 from flask_jwt_extended import JWTManager
-
+from flask_migrate import Migrate
 
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
@@ -10,7 +10,7 @@ from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 # imports/
 """
-Imports referentes as Libs Flask Restex,  SQLAlchemy, Marshmallow e JWT- São excenciais para a construção da aplicaçao
+Imports referentes as Libs Flask Restx,  SQLAlchemy, Marshmallow e JWT- São excenciais para a construção da aplicaçao
 Devido ao padrão de Projeto Factory e Blueprints  elas tem de ser importadas aqui e carregadas para os outros modulos. Do contrario
 são levantados erros de importação Circular.
 """
@@ -18,3 +18,7 @@ api = Api()
 db = SQLAlchemy()
 ma = Marshmallow()
 jwt = JWTManager()
+
+
+#Migrações de databases
+migrate = Migrate()
