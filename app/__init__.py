602e74bae368ec75db635c2fb9b6b9482e98f62e
Guilherme Correa <78979736+guilhermedcorrea@users.noreply.github.com>
2022-09-26 03:00:59 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,38 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-
+from flask import abort, jsonify
 from config import uri_database, secret_key
+
+
+#Handlers
+def register_handlers(app):
+    if app.config.get('DEBUG') is True:
+        app.logger.debug('Skipping error handlers in Debug mode')
+        return
+
+    @app.errorhandler(404)
+    def not_found_error(error):
+        return jsonify({"Error":"not found error"}), 404
+
+    @app.errorhandler(500)
+    def internal_error(error):
+        return jsonify({"Error":"internal error"}), 500
+
+    @app.errorhandler(500)
+    def ModuleNotFoundError(*args, **kwargs):
+        return jsonify({"Error":"Server Error"}), 500
+
+    @app.errorhandler(404)
+    def page_not_found(*args, **kwargs):
+        return jsonify({"Error":"EndPoint NotFound"}), 404
+   
+    @app.errorhandler(405)
+    def method_not_allowed_page(*args, **kwargs):
+  
+        return jsonify({"Error":"Method not found"}), 405
+        
+
 #Factory
 def create_app():
     """Declare APP e registra blueprints"""
@@ -18,14 +48,14 @@ def create_app():
     jwt.init_app(app)
     migrate.init_app(app, db)
    
+    
     with app.app_context():
         """Não mover esses imports para cima pois podem causar importação circular"""
         from .fretes.frete_produto import frete_bp
         from .admin.Admin import admin_bp
- 
+        register_handlers(app)
         app.register_blueprint(admin_bp)
         app.register_blueprint(frete_bp)
         
-       
 
     return app
